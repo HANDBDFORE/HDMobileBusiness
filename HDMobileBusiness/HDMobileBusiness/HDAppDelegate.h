2a8f08b62321e75364ec5e876023632bc252050f
Stone Fish <rocky.leo_stone@yahoo.com>
2012-07-30 13:10:12 +0800

    create project

---
+++ HDMobileBusiness/HDMobileBusiness/HDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HDAppDelegate.h
+//  HDMobileBusiness
+//
+//  Created by Plato on 7/30/12.
+//  Copyright (c) 2012 hand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
