46d04366eb23196810b20c276c269092dcb3e868
Stone Fish <rocky.leo_stone@yahoo.com>
2012-07-30 13:09:51 +0800

    create project

---
+++ HDMobileBusiness/HDMobileBusinessTests/HDMobileBusinessTests.h
@@ -0,0 +1,13 @@
+//
+//  HDMobileBusinessTests.h
+//  HDMobileBusinessTests
+//
+//  Created by Plato on 7/30/12.
+//  Copyright (c) 2012 hand. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HDMobileBusinessTests : SenTestCase
+
+@end
---
+++ HDMobileBusiness/HDMobileBusinessTests/HDMobileBusinessTests.m
@@ -0,0 +1,32 @@
+//
+//  HDMobileBusinessTests.m
+//  HDMobileBusinessTests
+//
+//  Created by Plato on 7/30/12.
+//  Copyright (c) 2012 hand. All rights reserved.
+//
+
+#import "HDMobileBusinessTests.h"
+
+@implementation HDMobileBusinessTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HDMobileBusinessTests");
+}
+
+@end
