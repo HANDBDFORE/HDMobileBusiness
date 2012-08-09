34103c9abb7d22e2d3c34010cb1147f7d8fad0a4
Stone Fish <rocky.leo_stone@yahoo.com>
2012-08-09 12:58:02 +0800

    完成coreStorage单元测试

--- HDMobileBusiness/HDMobileBusinessTests/HDMobileBusinessTests.m
+++ HDMobileBusiness/HDMobileBusinessTests/HDMobileBusinessTests.m
@@ -24,33 +24,76 @@
     [super tearDown];
 }
 
-- (void)testQuery
+-(void)testCoreStorage
 {
+    //insert
+    HDApprovalRecord * record1 = [[[HDApprovalRecord alloc]init] autorelease];
+    record1.recordId = @"test1";
+    record1.serverId = @"测试001";
+    record1.timeStamp = [NSDate dateWithTimeIntervalSince1970:0];
+    record1.recordTitle = @"i测试Title";
+    record1.recordStatus = HDRecordStatusNormal;
+    STAssertTrue([[HDCoreStorage shareStorage] insert:[NSArray arrayWithObject:record1]],@"insert failed");
+    HDApprovalRecord * record2 = [[[HDApprovalRecord alloc]init] autorelease];
+    record2.recordId = @"test2";
+    record2.serverId = @"测试002";
+    record2.timeStamp = [NSDate dateWithTimeIntervalSince1970:0];
+    record2.recordTitle = @"i测试Title";
+    record2.recordStatus = HDRecordStatusNormal;
+    [[HDCoreStorage shareStorage] sync:[NSArray arrayWithObject:record2]];
+    //query
     NSArray * resultList = [[HDCoreStorage shareStorage] query:nil];
-
-    //列表长度为1
-    TTDASSERT(resultList.count == 1);
+    //列表长度为2
+    STAssertTrue(resultList.count == 2,@"list count is:%i",resultList.count);
     //记录对象是HDApprovalRecord
     TTDASSERT([[resultList objectAtIndex:0] isKindOfClass:[HDApprovalRecord class]]);
     //数据校验
-    HDApprovalRecord * record = [resultList objectAtIndex:0];
-    STAssertEqualObjects(record.recordId, @"1", @"获取recordId失败");
-    STAssertEqualObjects(record.serverId, @"test001", @"获取serverId失败");
-    STAssertEqualObjects(record.comments, @"同意", @"获取comments失败");
-    STAssertEqualObjects(record.userId, @"2991", @"获取userId失败");
-    STAssertEqualObjects(record.userName, @"无", @"获取userName失败");
-    STAssertEqualObjects(record.timeStamp, [NSDate dateWithTimeIntervalSince1970:0], @"获取timeStamp失败");
-    STAssertEqualObjects(record.recordTitle, @"测试Title", @"获取recordTitle失败",record.recordTitle);
-    STAssertEqualObjects(record.recordCaption, @"测试Caption", @"获取recordiCaption失败",record.recordCaption);
-    STAssertEqualObjects(record.recordText, @"测试recordText", @"获取recordText失败",record.recordText);
-    STAssertEqualObjects(record.recordImagePath, @"bundle://test.png", @"获取recordImagePath失败",record.recordImagePath);
-    STAssertEqualObjects(record.exceptionMessage, @"记录已经被审批", @"获取exceptionMessage失败",record.exceptionMessage);
-    STAssertEqualObjects(record.docPageUrl, @"module/test.screen", @"获取docPageUrl失败",record.docPageUrl);
+    record1 = [resultList objectAtIndex:0];
+    record2 = [resultList objectAtIndex:1];
     
-    STAssertTrue(record.recordStatus == HDRecordStatusNormal,@"没有正确设置recordStatus",record.recordStatus);
-    STAssertTrue(record.storageStatus == HDStorageStatusNormal, @"没有正确设置storageStatus",record.storageStatus);
+    STAssertEqualObjects(record1.recordId, @"test1", @"获取recordId失败");
+    STAssertEqualObjects(record1.serverId, @"测试001", @"获取serverId失败");
+    STAssertEqualObjects(record1.timeStamp, [NSDate dateWithTimeIntervalSince1970:0], @"i获取timeStamp失败");
+    STAssertEqualObjects(record1.recordTitle, @"i测试Title", @"获取recordTitle失败",record1.recordTitle);
+    STAssertTrue(record1.recordStatus == HDRecordStatusNormal,@"没有正确设置recordStatus",record1.recordStatus);
+    STAssertTrue(record1.storageStatus == HDStorageStatusNormal, @"没有正确设置storageStatus",record1.storageStatus);
+    ///////////////
+    STAssertEqualObjects(record2.recordId, @"test2", @"获取recordId失败");
+    STAssertEqualObjects(record2.serverId, @"测试002", @"获取serverId失败");
     
-//    STFail(@"Unit tests are not implemented yet in HDMobileBusinessTests");
+    //update
+    record1.timeStamp = [NSDate dateWithTimeIntervalSince1970:10];
+    record1.recordTitle = @"u测试Title1";
+    record1.recordStatus = HDRecordStatusDifferent;
+    record1.storageStatus = HDStorageStatusUpdate;
+    STAssertTrue([[HDCoreStorage shareStorage] update:[NSArray arrayWithObject:record1]], @"update failed");
+    //sync update
+    record2.timeStamp = [NSDate dateWithTimeIntervalSince1970:10];
+    record2.recordTitle = @"u测试Title2";
+    record2.recordStatus = HDRecordStatusDifferent;
+    record2.storageStatus = HDStorageStatusUpdate;
+    [[HDCoreStorage shareStorage] sync:[NSArray arrayWithObject:record2]];
+    resultList = [[HDCoreStorage shareStorage] query:nil];
+    record1 = [resultList objectAtIndex:0];
+    record2 = [resultList objectAtIndex:1];
+    
+    STAssertEqualObjects(record1.timeStamp, [NSDate dateWithTimeIntervalSince1970:10], @"获取timeStamp失败");
+    STAssertEqualObjects(record1.recordTitle, @"u测试Title1", @"获取recordTitle失败",record1.recordTitle);
+    STAssertTrue(record1.recordStatus == HDRecordStatusDifferent,@"没有正确设置recordStatus",record1.recordStatus);
+    STAssertTrue(record1.storageStatus == HDStorageStatusNormal, @"没有正确设置storageStatus",record1.storageStatus);
+    ////////////////
+    STAssertEqualObjects(record2.timeStamp, [NSDate dateWithTimeIntervalSince1970:10], @"获取timeStamp失败");
+    STAssertEqualObjects(record2.recordTitle, @"u测试Title2", @"获取recordTitle失败",record1.recordTitle);
+    STAssertTrue(record2.recordStatus == HDRecordStatusDifferent,@"没有正确设置recordStatus",record1.recordStatus);
+    STAssertTrue(record2.storageStatus == HDStorageStatusNormal, @"没有正确设置storageStatus",record1.storageStatus);
+    
+    //remove
+    STAssertTrue([[HDCoreStorage shareStorage] remove:[NSArray arrayWithObject:record1]], @"remove failed");
+    record2.storageStatus = HDStorageStatusRemove;
+    [[HDCoreStorage shareStorage] sync:[NSArray arrayWithObject:record2]];
+    
+    STAssertTrue(![[HDCoreStorage shareStorage] query:nil], @"删除记录失败") ;
 }
+
 
 @end
