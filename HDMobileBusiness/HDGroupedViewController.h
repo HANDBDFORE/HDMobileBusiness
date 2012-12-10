fb5ad390d49f23bd00f7d628010b1b1e919c7c5e
Stone Fish <410423847@qq.com>
2012-12-10 13:07:54 +0800

    添加分组

---
+++ HDMobileBusiness/HDGroupedViewController.h
@@ -0,0 +1,13 @@
+//
+//  HDGroupedViewController.h
+//  HDMobileBusiness
+//
+//  Created by Plato on 12/6/12.
+//  Copyright (c) 2012 hand. All rights reserved.
+//
+
+#import <Three20UI/Three20UI.h>
+
+@interface HDGroupedViewController : TTTableViewController
+
+@end
