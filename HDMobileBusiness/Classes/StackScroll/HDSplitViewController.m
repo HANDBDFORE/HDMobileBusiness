3551d602013963cc9eaaf650af6a75c62bb60c34
oomhj <mr_haojie_ma@126.com>
2012-12-11 17:21:47 +0800

    手势滑动

--- HDMobileBusiness/Classes/StackScroll/HDSplitViewController.m
+++ HDMobileBusiness/Classes/StackScroll/HDSplitViewController.m
@@ -39,7 +39,7 @@
         
         _leftViewController = [leftViewController retain];
         _leftView = _leftViewController.view;
-        [self resizeSubViews];
+        [self resizeSubViews:YES];
         [self.view insertSubview:_leftView atIndex:0];
     }
 }
@@ -53,11 +53,55 @@
         _rightViewController = [rightViewController retain];
         _rightView = _rightViewController.view;
         _rightView.tag = 201;
-        [self resizeSubViews];
+        [self resizeSubViews:NO];
+        
         [self.view insertSubview:_rightView atIndex:1];
     }
+    UISwipeGestureRecognizer* recognizer;
+    recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
+    [recognizer setDirection:(UISwipeGestureRecognizerDirectionRight)];
+    [_rightView addGestureRecognizer:recognizer];
+    [recognizer release];
+    recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
+    [recognizer setDirection:(UISwipeGestureRecognizerDirectionLeft)];
+    [_rightView addGestureRecognizer:recognizer];
+    [recognizer release];
 }
 
+- (void)handleSwipeFrom:(UISwipeGestureRecognizer*)recognizer {
+    // 触发手勢事件后，在这里作些事情
+    if (CGRectGetWidth(self.view.bounds)!=1024) {
+        if(recognizer.direction == UISwipeGestureRecognizerDirectionRight){
+            [UIView beginAnimations:@"moveright" context:nil];
+            //动画持续时间
+            [UIView setAnimationDuration:0.5];
+            //设置动画的回调函数，设置后可以使用回调方法
+            [UIView setAnimationDelegate:self];
+            //设置动画曲线，控制动画速度
+            [UIView  setAnimationCurve:UIViewAnimationCurveEaseOut];
+            //设置动画方式，并指出动画发生的位置
+            [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:_rightView  cache:YES];
+            _rightView.frame = CGRectMake(_leftViewWidth, 0,_rightView.frame.size.width,_rightView.frame.size.height);
+            //提交UIView动画
+            [UIView commitAnimations];
+            
+        }else if (recognizer.direction == UISwipeGestureRecognizerDirectionLeft){
+            [UIView beginAnimations:@"moveleft" context:nil];
+            //动画持续时间
+            [UIView setAnimationDuration:0.5];
+            //设置动画的回调函数，设置后可以使用回调方法
+            [UIView setAnimationDelegate:self];
+            //设置动画曲线，控制动画速度
+            [UIView  setAnimationCurve:UIViewAnimationCurveEaseOut];
+            //设置动画方式，并指出动画发生的位置
+            [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:_rightView  cache:YES];
+            CGFloat rightViewWidth = 1024-_leftViewWidth;
+            _rightView.frame = CGRectMake(CGRectGetWidth(self.view.bounds)-rightViewWidth, 0,_rightView.frame.size.width,_rightView.frame.size.height);
+            //提交UIView动画
+            [UIView commitAnimations];
+        };
+    }
+}
 #pragma -mark life cycle
 -(void)viewWillAppear:(BOOL)animated
 {
@@ -81,32 +125,35 @@
 	[_rightViewController viewDidAppear:NO];
 }
 
--(void)resizeSubViews
+-(void)resizeSubViews:(BOOL)animated
 {
-    CGFloat leftViewWidth = 0;
-    
-    if (_leftViewWidth) {
-        leftViewWidth = _leftViewWidth;
-    }else{
-        leftViewWidth = self.view.frame.size.width * _leftViewPercentWidth;
-    }
-    UIColor * borderColor = TTSTYLEVAR(splitBorderColor);
-        
-    _leftView.frame = CGRectMake(0, 0, leftViewWidth, CGRectGetHeight(self.view.bounds));
+    CGFloat rightViewWidth = 1024-_leftViewWidth;
+    _leftView.frame = CGRectMake(0, 0, _leftViewWidth, CGRectGetHeight(self.view.bounds));
     _leftView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
-    [_leftView.layer setBorderWidth:1.5f];
-    [_leftView.layer setCornerRadius:5];
-    [_leftView.layer setBorderColor:borderColor.CGColor];
-    
-    
-    _rightView.frame = CGRectMake(leftViewWidth, 0, CGRectGetWidth(self.view.bounds)-leftViewWidth, CGRectGetHeight(self.view.bounds));
-    _rightView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-    [_rightView.layer setCornerRadius:5];
+    if (animated) {
+        [UIView beginAnimations:@"move" context:nil];
+        //动画持续时间
+        [UIView setAnimationDuration:0.5];
+        //设置动画的回调函数，设置后可以使用回调方法
+        [UIView setAnimationDelegate:self];
+        //设置动画曲线，控制动画速度
+        [UIView  setAnimationCurve:UIViewAnimationCurveEaseOut];
+        //设置动画方式，并指出动画发生的位置
+        [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:_rightView  cache:YES];
+        _rightView.frame = CGRectMake(CGRectGetWidth(self.view.bounds)-rightViewWidth, 0,rightViewWidth, CGRectGetHeight(self.view.bounds));
+        _rightView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+        //提交UIView动画
+        [UIView commitAnimations];
+    }else{
+        _rightView.frame = CGRectMake(CGRectGetWidth(self.view.bounds)-rightViewWidth, 0,rightViewWidth, CGRectGetHeight(self.view.bounds));
+        _rightView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    }
 }
 
 -(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
 	[_leftViewController didRotateFromInterfaceOrientation:fromInterfaceOrientation];
 	[_rightViewController didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    [self resizeSubViews:YES];
 }
 
 -(void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
